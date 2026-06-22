599bedd9aef0950a9a68719250e120a7655cbbd2
Ambarubale6061 <ambarubale3333@gmail.com>
2026-06-22 15:52:05 +0530

    Create LastOccurrence.java

---
+++ Programming 100 to 200/LastOccurrence.java
@@ -0,0 +1,21 @@
+public class LastOccurrence {
+    public static int lastOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 2, 3, 4 };
+        System.out.println(lastOccurrence(arr, 2)); // 3
+    }
+}
