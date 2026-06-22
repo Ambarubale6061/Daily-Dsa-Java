ab6eaea4444ec4fe3cc6223cb34d317a9bd8b7f3
Ambarubale6061 <ambarubale3333@gmail.com>
2026-06-22 15:55:38 +0530

    Create 111_MinimumInRotatedArray.java

---
+++ Programming 100 to 200/111_MinimumInRotatedArray.java
@@ -0,0 +1,15 @@
+public class MinimumInRotatedArray {
+    public static int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > nums[r]) l = m + 1;
+            else r = m;
+        }
+        return nums[l];
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(findMin(nums)); // 1
+    }
+}
