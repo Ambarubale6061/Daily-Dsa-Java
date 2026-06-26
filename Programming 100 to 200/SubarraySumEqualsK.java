9807f807a13d5a9406160b16fd441a2c57b2b882
Ambarubale6061 <ambarubale3333@gmail.com>
2026-06-26 21:05:12 +0530

    Create SubarraySumEqualsK.java

---
+++ Programming 100 to 200/SubarraySumEqualsK.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+        for (int num : nums) {
+            sum += num;
+            count += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1 };
+        System.out.println(subarraySum(nums, 2)); // 2
+    }
+}
