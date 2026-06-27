39fbfeae453b069a071d674923cbef8ba8e702e0
Ambarubale6061 <ambarubale3333@gmail.com>
2026-06-27 17:41:15 +0530

    Create LowestCommonAncestor.java

---
+++ Programming 100 to 200/LowestCommonAncestor.java
@@ -0,0 +1,28 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class LowestCommonAncestor {
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null)
+            return root;
+        return left != null ? left : right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        TreeNode lca = lowestCommonAncestor(root, root.left, root.right);
+        System.out.println(lca.val); // 3
+    }
+}
