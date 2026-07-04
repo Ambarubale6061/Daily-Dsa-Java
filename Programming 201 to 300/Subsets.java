589fc131dbd2bb90ec5eed6fe2b15da3329be7c9
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-04 19:05:56 +0530

    Create Subsets.java

---
+++ Programming 201 to 300/Subsets.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Subsets {
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void backtrack(int[] nums, int start, List<Integer> temp, List<List<Integer>> res) {
+        res.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(nums, i + 1, temp, res);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subsets(new int[] { 1, 2, 3 }));
+    }
+}
