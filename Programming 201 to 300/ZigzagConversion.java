37a1d546dda31db9c1d1ffc947acfa1f527b5d3e
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-06 19:03:37 +0530

    Create ZigzagConversion.java

---
+++ Programming 201 to 300/ZigzagConversion.java
@@ -0,0 +1,25 @@
+public class ZigzagConversion {
+    public static String convert(String s, int numRows) {
+        if (numRows == 1)
+            return s;
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            rows[i] = new StringBuilder();
+        int curRow = 0;
+        boolean goingDown = false;
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+            if (curRow == 0 || curRow == numRows - 1)
+                goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder sb : rows)
+            res.append(sb);
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 3)); // PAHNAPLSIIGYIR
+    }
+}
