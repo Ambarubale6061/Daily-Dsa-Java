df2e0827a965b8cac1e3f385e97c4cef05d63e30
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-06 19:05:27 +0530

    Create ReverseString.java

---
+++ Programming 201 to 300/ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+        reverseString(s);
+        System.out.println(java.util.Arrays.toString(s));
+    }
+}
