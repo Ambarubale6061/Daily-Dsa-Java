030b208e5418341a10cb7a2cdfb7253da9b922b7
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-08 18:47:06 +0530

    Create StringMatching.java

---
+++ Programming 201 to 300/StringMatching.java
@@ -0,0 +1,20 @@
+public class StringMatching {
+    // Naive string matching
+    public static int naiveSearch(String text, String pattern) {
+        int n = text.length(), m = pattern.length();
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (text.charAt(i + j) != pattern.charAt(j))
+                    break;
+            }
+            if (j == m)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(naiveSearch("hello world", "world")); // 6
+    }
+}
