4fa19171c571aa20d9b605ff604d7f741ab27f79
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-10 16:17:21 +0530

    Create ModInverse.java

---
+++ Programming 201 to 300/ModInverse.java
@@ -0,0 +1,23 @@
+public class ModInverse {
+    public static long modInverse(long a, long m) {
+        long m0 = m, y = 0, x = 1;
+        if (m == 1)
+            return 0;
+        while (a > 1) {
+            long q = a / m;
+            long t = m;
+            m = a % m;
+            a = t;
+            t = y;
+            y = x - q * y;
+            x = t;
+        }
+        if (x < 0)
+            x += m0;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(modInverse(3, 11)); // 4
+    }
+}
