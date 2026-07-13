76c88ce70768e32e6ff61bf8078ab966c4846a4e
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-13 17:15:39 +0530

    Create TrappingRainWater.java

---
+++ Programming 201 to 300/TrappingRainWater.java
@@ -0,0 +1,26 @@
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        int l = 0, r = height.length - 1, lMax = 0, rMax = 0, ans = 0;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lMax)
+                    lMax = height[l];
+                else
+                    ans += lMax - height[l];
+                l++;
+            } else {
+                if (height[r] >= rMax)
+                    rMax = height[r];
+                else
+                    ans += rMax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        System.out.println(trap(height)); // 6
+    }
+}
