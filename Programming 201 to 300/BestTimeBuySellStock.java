6945b358caa76261019db00c85113e535535351b
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-13 17:12:01 +0530

    Create BestTimeBuySellStock.java

---
+++ Programming 201 to 300/BestTimeBuySellStock.java
@@ -0,0 +1,15 @@
+public class BestTimeBuySellStock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for (int p : prices) {
+            minPrice = Math.min(minPrice, p);
+            maxProfit = Math.max(maxProfit, p - minPrice);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfit(prices)); // 5
+    }
+}
