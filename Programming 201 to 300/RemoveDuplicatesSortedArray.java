f989269aa1e1110f3d4dd9df76939adc30eea849
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-15 15:34:52 +0530

    Create RemoveDuplicatesSortedArray.java

---
+++ Programming 201 to 300/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,18 @@
+public class RemoveDuplicatesSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j])
+                nums[++j] = nums[i];
+        }
+        return j + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 2 };
+        int len = removeDuplicates(nums);
+        System.out.println(len); // 2
+    }
+}
