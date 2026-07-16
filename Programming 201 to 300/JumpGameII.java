8220d288dd8dfaf6b90041113f368be6277f15bb
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-16 13:01:51 +0530

    Create JumpGameII.java

---
+++ Programming 201 to 300/JumpGameII.java
@@ -0,0 +1,18 @@
+public class JumpGameII {
+    public static int jump(int[] nums) {
+        int jumps = 0, curEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 1, 1, 4 };
+        System.out.println(jump(nums)); // 2
+    }
+}
