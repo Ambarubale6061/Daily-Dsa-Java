137478a6ba2c3eb75aaed06e5770799ad6f37c1c
Ambar Ubale <ambarubale3333@gmail.com>
2026-07-29 18:28:28 +0530

    Create BottomViewBinaryTree.java

---
+++ Programming 301 to 400/BottomViewBinaryTree.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BottomViewBinaryTree {
+    static class Pair {
+        TreeNode node;
+        int hd;
+
+        Pair(TreeNode n, int h) {
+            node = n;
+            hd = h;
+        }
+    }
+
+    public static List<Integer> bottomView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 0));
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            map.put(p.hd, p.node.val);
+            if (p.node.left != null)
+                q.add(new Pair(p.node.left, p.hd - 1));
+            if (p.node.right != null)
+                q.add(new Pair(p.node.right, p.hd + 1));
+        }
+        res.addAll(map.values());
+        return res;
+    }
+}
