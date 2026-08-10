d2ae447a09c2bdc65fdd2287a153778e888c0a4d
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-10 15:46:57 +0530

    Create JumpPointers.java

---
+++ Programming 401 to 500/JumpPointers.java
@@ -0,0 +1,10 @@
+// 431_JumpPointers.java
+public class JumpPointers {
+  // Same as binary lifting; "Jump pointers" often refer to binary lifting for LCA
+  // or level ancestors.
+  // Reuse BinaryLifting class. This file can be a placeholder to illustrate the
+  // concept.
+  public static void main(String[] args) {
+    System.out.println("Jump pointers / binary lifting concept.");
+  }
+}
