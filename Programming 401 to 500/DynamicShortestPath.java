8772390708e957642cbd1372950ea4a346c17160
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-11 18:18:28 +0530

    Create DynamicShortestPath.java

---
+++ Programming 401 to 500/DynamicShortestPath.java
@@ -0,0 +1,10 @@
+// 444_DynamicShortestPath.java
+public class DynamicShortestPath {
+  // Placeholder: dynamic shortest path
+  public void addEdge(int u, int v, int w) {
+  }
+
+  public int query(int u, int v) {
+    return 0;
+  }
+}
