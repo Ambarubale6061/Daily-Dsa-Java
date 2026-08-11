fd00abe9732afd992f2dff94333e297420e3a53f
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-11 18:18:00 +0530

    Create DynamicConnectivity.java

---
+++ Programming 401 to 500/DynamicConnectivity.java
@@ -0,0 +1,13 @@
+// 439_DynamicConnectivity.java
+public class DynamicConnectivity {
+  // Placeholder: dynamic connectivity with link/cut or offline
+  boolean connected(int u, int v) {
+    return false;
+  }
+
+  void link(int u, int v) {
+  }
+
+  void cut(int u, int v) {
+  }
+}
