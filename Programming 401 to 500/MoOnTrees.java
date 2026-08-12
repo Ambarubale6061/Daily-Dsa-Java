06c5bf1afc72b1af141188faaa5ce37a6c98f72b
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-12 19:38:40 +0530

    Create MoOnTrees.java

---
+++ Programming 401 to 500/MoOnTrees.java
@@ -0,0 +1,11 @@
+
+// 437_MoOnTrees.java
+import java.util.*;
+
+public class MoOnTrees {
+  // Placeholder: Mo's algorithm on tree using Euler tour and block decomposition
+  public static int[] process(int n, List<Integer>[] adj, int[][] queries) {
+    // Not implemented fully
+    return new int[queries.length];
+  }
+}
