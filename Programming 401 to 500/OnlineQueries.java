b5494a42b83647f5a9be93102bf8a5cb68b8ddbf
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-12 19:40:16 +0530

    Create OnlineQueries.java

---
+++ Programming 401 to 500/OnlineQueries.java
@@ -0,0 +1,8 @@
+// 441_OnlineQueries.java
+public class OnlineQueries {
+  // Example: online with persistent segment tree
+  // Placeholder
+  public int query(int l, int r) {
+    return 0;
+  }
+}
