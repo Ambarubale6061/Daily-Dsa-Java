14a8215085373b415f8ca9e55ded9e18df0dc7ba
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-12 19:37:46 +0530

    Create KShortestPaths.java

---
+++ Programming 401 to 500/KShortestPaths.java
@@ -0,0 +1,10 @@
+
+// 447_KShortestPaths.java
+import java.util.*;
+
+public class KShortestPaths {
+  // Placeholder: Eppstein's algorithm / Yen's algorithm
+  public static List<List<Integer>> kShortestPaths(int[][] graph, int s, int t, int k) {
+    return new ArrayList<>();
+  }
+}
