eb50bcb9e4c23103122e480ab3d50124ce2439b2
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-14 19:48:38 +0530

    Create KnightShortestPath.java

---
+++ Programming 401 to 500/KnightShortestPath.java
@@ -0,0 +1,10 @@
+
+// 460_KnightShortestPath.java
+import java.util.*;
+
+public class KnightShortestPath {
+  public static int minKnightMoves(int n, int x, int y) {
+    // BFS on infinite board but bounded
+    return 0;
+  }
+}
