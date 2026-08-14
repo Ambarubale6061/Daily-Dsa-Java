37267327e4966b849680103fa3be573ed5544807
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-14 19:46:12 +0530

    Create BeamSearch.java

---
+++ Programming 401 to 500/BeamSearch.java
@@ -0,0 +1,10 @@
+
+// 451_BeamSearch.java
+import java.util.*;
+
+public class BeamSearch {
+  // Simple beam search for sequence generation
+  public static List<Integer> beamSearch(int beamWidth, int maxLen) {
+    return null;
+  }
+}
