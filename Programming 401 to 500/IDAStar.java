d7c602aa0919b03ad5256fa8e728681bdb9e695c
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-17 17:56:49 +0530

    Create IDAStar.java

---
+++ Programming 401 to 500/IDAStar.java
@@ -0,0 +1,10 @@
+
+// 452_IDAStar.java
+import java.util.*;
+
+public class IDAStar {
+  // IDA* for 15-puzzle like problems, placeholder
+  public static int idaStarSearch(int startState, int goal) {
+    return 0;
+  }
+}
