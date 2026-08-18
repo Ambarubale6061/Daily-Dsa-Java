c06aa0feef97f012aa056038109531c7aa9058d5
Ambar Ubale <ambarubale3333@gmail.com>
2026-08-18 20:59:33 +0530

    Create YenAlgorithm.java

---
+++ Programming 401 to 500/YenAlgorithm.java
@@ -0,0 +1,10 @@
+
+// 448_YenAlgorithm.java
+import java.util.*;
+
+public class YenAlgorithm {
+  // Yen's algorithm for k shortest loopless paths
+  public static List<List<Integer>> yenKShortest(int n, List<int[]>[] adj, int s, int t, int K) {
+    return new ArrayList<>();
+  }
+}
